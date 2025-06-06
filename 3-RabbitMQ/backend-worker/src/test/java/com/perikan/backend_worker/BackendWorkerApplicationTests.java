dc48771851272d43d7fb062bfebedbdfef0dd5ff
henrickyL <henrickyl1@gmail.com>
2025-06-06 16:18:23 -0300

    base projects

---
+++ 3-RabbitMQ/backend-worker/src/test/java/com/perikan/backend_worker/BackendWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perikan.backend_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
